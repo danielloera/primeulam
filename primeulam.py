650c12e77f4bcd8b6914bf95e48b42aa682b1526
Daniel Matthew Loera <danielloera@utexas.edu>
2018-03-14 02:09:08 -0500

    added custom shape, and starting integer

--- primeulam.py
+++ primeulam.py
@@ -6,15 +6,26 @@ LEFT = 1
 DOWN = 2
 RIGHT = 3
 
+CIRCLE = "circle"
+SQUARE = "square"
+TRIANLGE = "triangle"
+
+CIRCLE_NUM = 0
+SQUARE_NUM = 1
+TRIANLGE_NUM = 2
+
 next_movement = {
             UP: LEFT,
             LEFT: DOWN,
             DOWN: RIGHT,
             RIGHT: UP
-                            }
+}
 
-def in_bounds(x, y, size):
-    return x >= 0 and x < size and y >= 0 and y < size
+shape_convert = {
+            CIRCLE: CIRCLE_NUM,
+            SQUARE: SQUARE_NUM,
+            TRIANLGE: TRIANLGE_NUM
+}
 
 def neighbor(curr_movement, x, y, board):
     if curr_movement == UP:
@@ -34,15 +45,14 @@ def update(curr_movement, x, y):
         return x + 1, y
     return x, y + 1
 
-def spiralize(board):
+def spiralize(board, num):
     size = len(board)
     x, y = size // 2, size // 2
-    num = 1
     board[x][y] = num
     y += 1
     num += 1
     movement = UP
-    while in_bounds(x, y, size):
+    while x >= 0 and x < size and y >= 0 and y < size:
         if neighbor(movement, x, y, board) is not None:
             board[x][y] = num
             x, y = update(movement, x, y)
@@ -62,41 +72,66 @@ def primes_sieve(limit):
                 a[n] = False 
     return ans
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument("-size", type=int, default=201)
-parser.add_argument("-bg", type=str, default="blue")
-parser.add_argument("-color", type=str, default="red")
-parser.add_argument("-radius", type=int, default=1)
+def get_shape(x, y, multiplier, shape, shape_size):
+    x, y = x * multiplier, y * multiplier
+    if shape == CIRCLE_NUM:
+        return Circle(Point(x, y), shape_size)
+    if shape == SQUARE_NUM:
+        return Rectangle(
+                Point(x - shape_size, y - shape_size),
+                Point(x + shape_size, y + shape_size))
+    half_size = shape_size // 2
+    return Polygon(
+            Point(x, y),
+            Point(x - multiplier, y + multiplier),
+            Point(x + multiplier, y + multiplier))
 
-args = parser.parse_args()
 
-if args.size % 2 != 1:
-    raise Exception("please ensure size is odd.")
+def main():
 
-multiplier = 2 * args.radius
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("-size", type=int, default=201)
+    parser.add_argument("-bg", type=str, default="blue")
+    parser.add_argument("-color", type=str, default="red")
+    parser.add_argument("-shape_size", type=float, default=1)
+    parser.add_argument("-shape", type=str, default="circle")
+    parser.add_argument("-start", type=int, default=1)
 
-primes = primes_sieve(args.size ** 2)
+    args = parser.parse_args()
 
-board = [[None for i in range(args.size)] for j in range(args.size)]
-spiralize(board)
+    if args.size % 2 != 1:
+        raise Exception("please ensure size is odd.")
 
-win = GraphWin("Prime Ulam Spiral (size {}) ".format(args.size),
-                args.size * multiplier,
-                args.size * multiplier,
-                autoflush=False)
-win.setBackground(args.bg)
+    multiplier = 2 * args.shape_size
+    shape = shape_convert[args.shape]
 
-for i in range(len(board)):
-    for j in range(len(board)):
-        if board[i][j] in primes:
-            c = Circle(Point(i* multiplier, j * multiplier), args.radius)
-            c.setFill(args.color)
-            c.setOutline(args.color)
-            c.draw(win)
+    primes = primes_sieve(args.size ** 2)
 
-# To force instant drawing
-for i in range(1):
+    board = [[None for i in range(args.size)] for j in range(args.size)]
+    spiralize(board, args.start)
+
+    win = GraphWin("Prime Ulam Spiral (size {}) ".format(args.size),
+                    args.size * multiplier,
+                    args.size * multiplier,
+                    autoflush=False)
+    win.setBackground(args.bg)
+
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] in primes:
+                s = get_shape(i, j,
+                                multiplier,
+                                shape,
+                                args.shape_size)
+                s.setFill(args.color)
+                s.setOutline(args.color)
+                s.draw(win)
+
+    # force instant draw            
     update(0,0,1)
 
-win.getMouse()
-win.close()
+    win.getMouse()
+    win.close()
+
+if __name__ == '__main__':
+    main()
